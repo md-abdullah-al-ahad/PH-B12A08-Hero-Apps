438084ccaab9b71e5d5516785c39c80596e3fb31
Md. Abdullah Al Ahad <abdullah.ruet22@gmail.com>
2025-10-09 03:26:06 +0600

    add toast notifications

--- src/pages/Installations/Installations.jsx
+++ src/pages/Installations/Installations.jsx
@@ -59,7 +59,7 @@ const Installations = () => {
         <div className="flex flex-col gap-3">
           {sortedApps.length === 0 ? (
             <div className="text-center text-gray-500 py-10">
-              No installed apps found.
+              <p className="mb-10">No installed apps found.</p>
               <br />
               <Link
                 to="/Apps"
--- src/pages/Root/Root.jsx
+++ src/pages/Root/Root.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { Outlet } from "react-router";
 import Navbar from "../../Components/Navbar/Navbar";
 import Footer from "../../Components/Footer/Footer";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Root = () => {
   const [installedApps, setInstalledApps] = useState([]);
@@ -9,17 +11,22 @@ const Root = () => {
   const handleInstallApp = (appId) => {
     if (!installedApps.includes(appId)) {
       setInstalledApps([...installedApps, appId]);
+      toast.success("App installed successfully", { position: "top-right" });
     }
   };
 
   const handleUninstallApp = (appId) => {
     setInstalledApps(installedApps.filter((id) => id !== appId));
+    toast.info("App uninstalled", { position: "top-right" });
   };
 
   return (
     <div className="">
       <Navbar></Navbar>
-      <Outlet context={{ installedApps, handleInstallApp, handleUninstallApp }} />
+      <Outlet
+        context={{ installedApps, handleInstallApp, handleUninstallApp }}
+      />
+      <ToastContainer />
       <Footer></Footer>
     </div>
   );
